94d4276f7a1b0752c22930bd908cbf31cb9899eb
Ocknyer <kdu4045@naver.com>
2023-04-24 00:41:17 +0900

    fix: store 기능 일부 수정

--- src/components/SetList.svelte
+++ src/components/SetList.svelte
@@ -1,9 +1,22 @@
 <script>
+  import { DataTable } from 'carbon-components-svelte';
+
   export let setlist;
 </script>
 
 <div>
-  <pre>
-    {setlist}
-  </pre>
+  <DataTable
+    headers={[
+      { key: 'bpm', value: 'BPM' },
+      { key: '1/2', value: '1/2' },
+      { key: '1/4', value: '1/4' },
+      { key: '1/8', value: '1/8' },
+      { key: '1/8T', value: '1/8T' },
+      { key: '1/16', value: '1/16' },
+      { key: '1/32', value: '1/32' },
+      { key: '1/64', value: '1/64' },
+      { key: '1/128', value: '1/128' },
+    ]}
+    rows={setlist[0]}
+  />
 </div>
